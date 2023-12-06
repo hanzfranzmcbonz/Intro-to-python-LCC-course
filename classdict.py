6d64c758bf81bfc74aede6604f46bf2e7a4029a4
hanzfranzmcbonz <146770980+hanzfranzmcbonz@users.noreply.github.com>
2023-12-06 15:10:37 -0500

    added Comments on classdict

--- classdict.py
+++ classdict.py
@@ -1,3 +1,3 @@
-roomNum = {"CS101": "3004", "CS102": "4501", "CS103": "6755", "NT110": "1244", "CM241": "1411"}
-teacherName = {"CS101": "Haynes", "CS102": "Alvarado", "CS103": "Rich", "NT110": "Burke", "CM241": "Lee"}
-classTime = {"CS101": "8:00 AM", "CS102": "9:00 AM", "CS103": "10:00 AM", "NT110": "11:00 AM", "CM241": "1:00 PM"}
+roomNum = {"CS101": "3004", "CS102": "4501", "CS103": "6755", "NT110": "1244", "CM241": "1411"} #Dict for room number set to course number
+teacherName = {"CS101": "Haynes", "CS102": "Alvarado", "CS103": "Rich", "NT110": "Burke", "CM241": "Lee"} # Dict for teachers name set to course number
+classTime = {"CS101": "8:00 AM", "CS102": "9:00 AM", "CS103": "10:00 AM", "NT110": "11:00 AM", "CM241": "1:00 PM"} # Dict for Time of class set to course number
