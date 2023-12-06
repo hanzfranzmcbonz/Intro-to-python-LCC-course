e0faafb560579e520810e83b099c24c8f569ce4b
hanzfranzmcbonz <146770980+hanzfranzmcbonz@users.noreply.github.com>
2023-12-06 14:56:16 -0500

    Added in teacher name and class time

--- classdict.py
+++ classdict.py
@@ -1,2 +1,3 @@
 roomNum = {"CS101": "3004", "CS102": "4501", "CS103": "6755", "NT110": "1244", "CM241": "1411"}
-teacherName = {
+teacherName = {"CS101": "Haynes", "CS102": "Alvarado", "CS103": "Rich", "NT110": "Burke", "CM241": "Lee"}
+classTime = {"CS101": "8:00 AM", "CS102": "9:00 AM", "CS103": "10:00 AM", "NT110": "11:00 AM", "CM241": "1:00 PM"}
