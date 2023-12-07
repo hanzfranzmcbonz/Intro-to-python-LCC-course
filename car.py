36b7249bf57ce6f3edce0e399d8bcc0eb349b58c
hanzfranzmcbonz <146770980+hanzfranzmcbonz@users.noreply.github.com>
2023-12-06 21:59:32 -0500

    Created car class

---
+++ car.py
@@ -0,0 +1,17 @@
+class Car:
+    
+    def __init__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
+    
+    def accelerate(self):
+        self.__speed += 5
+    
+    def brake(slef):
+        self.__speed -= 5
+        if self.__speed < 0:
+            self.__speed = 0
+    
+    def get_speed(slef):
+        return self.__speed
