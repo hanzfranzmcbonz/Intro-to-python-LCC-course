7b2e756abc1c12053186be124bab8df9fd386548
hanzfranzmcbonz <146770980+hanzfranzmcbonz@users.noreply.github.com>
2023-12-06 22:01:24 -0500

    Added comments to car.py

--- car.py
+++ car.py
@@ -1,17 +1,22 @@
+# Creates a class called Car
 class Car:
     
+    # Initializes the arguments
     def __init__(self, year_model, make):
         self.__year_model = year_model
         self.__make = make
         self.__speed = 0
     
+    # Adds 5 speed when called
     def accelerate(self):
         self.__speed += 5
     
+    # subtracts 5 speed when called
     def brake(slef):
         self.__speed -= 5
         if self.__speed < 0:
             self.__speed = 0
     
+    # Gets Speed 
     def get_speed(slef):
         return self.__speed
